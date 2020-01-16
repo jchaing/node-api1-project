ac640208d52475a2ed818d960c2c4a4dc340e4ef
john <john@spinningplates.org>
2020-01-15 16:24:46 -0800

    POST functionality complete

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ server.get('/api/users', (req, res) => {
   db.find()
     .then(users => {
       res.status(200).json(users);
-      console.log(req.params);
     })
     .catch(err => {
       res.status(500).json({
@@ -54,27 +53,24 @@ server.get('/api/users/:id', (req, res) => {
 
 // When the client makes a POST request to /api/users:
 
-// server.post('/api/users', (req, res) => {
-//   const userInfo = req.body;
-//   const {name, bio} = req.body;
-//   console.log(userInfo)
-//   // console.log(bio)
+server.post('/api/users', (req, res) => {
+  const userInfo = req.body;
+  const { name, bio } = req.body;
 
-//   db.insert(userInfo)
-//     .then(user => {
-//       if (name && bio) {
-//         res.status(201).json({ user });
-//       } else {
-//         res.status(400).json({
-//           errorMessage: 'Please provide name and bio for the user.'
-//         });
-//       }
-//     })
-//     .catch(err => {
-//       res.status(500).json({
-//         errorMessage:
-//           'There was an error while saving the user to the database',
-//         err
-//       });
-//     });
-// });
+  name && bio
+    ? db
+        .insert(userInfo)
+        .then(user => {
+          res.status(201).json({ ...user, name, bio });
+        })
+        .catch(err => {
+          res.status(500).json({
+            errorMessage:
+              'There was an error while saving the user to the database',
+            err
+          });
+        })
+    : res.status(400).json({
+        errorMessage: 'Please provide name and bio for the user.'
+      });
+});
